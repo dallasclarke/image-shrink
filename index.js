39ec1600fa6cba89e67bc097d0c9b4f1d493df3f
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-09-06 01:16:27 -0700

    Setting up environment & checking platform.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow } = require("electron");
 
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -7,10 +12,26 @@ function createMainWindow() {
     title: "Image Shrink",
     width: 500,
     height: 600,
-    icon: `${__dirname}/assets/icons/Icon_256x256.png`
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    webPreferences: {
+      nativeWindowOpen: true,
+    },
   });
 
   mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
